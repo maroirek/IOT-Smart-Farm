3f1efcc8cab11523a2e0bcc6b86051df692d1f3f
MarouaREKROUK <maroua.rekrouk@g.enp.edu.dz>
2022-09-21 21:27:10 +0100

    Update2

--- iot-smart-farm.c
+++ iot-smart-farm.c
@@ -28,6 +28,26 @@ const char* ventilo_topic = "esp32/air";
 const char* servo_topic    ="esp32/servo";
 // const char* led_topic = "esp32/light";
 
+// Initializes the espClient
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+const int wet  = 1615;   //you need to replace this value with Value_1
+const int dry= 3450;  //you need to replace this value with Value_2
+int Liquid_level=0; 
+
+int sMValue = 0;
+int soilmoisturepercent=0;
+
+const int ANALOGPIN= 35 ;
+
+MQ135 gasSensor = MQ135(ANALOGPIN);
+DHT dht(DHTPIN, DHTTYPE);
+
+unsigned long previousMillis = 0;
+unsigned long interval = 30000;
+
+
 const char* humidity_topic = "serre/humidity";
 const char* temperature_topic ="serre/temperature";
 const char* soil_topic = "serre/soli";
